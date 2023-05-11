dd2b92c48eb03ed3fbd79c2126a27b7fc26cffab
JanHumphrey <sjanhumphrey@gmail.com>
2023-05-11 11:01:17 +0800

    add is_admin in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,35 +10,56 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+  use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    'name',
+    'email',
+    'password',
+    'is_admin' // add this field to the fillable array
+  ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var array<int, string>
+   */
+  protected $hidden = [
+    'password',
+    'remember_token',
+  ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array<string, string>
+   */
+  protected $casts = [
+    'email_verified_at' => 'datetime',
+  ];
+
+  public function carts()
+  {
+    return $this->hasMany(Cart::class);
+  }
+
+  public function orders()
+  {
+    return $this->hasMany(Order::class);
+  }
+
+  /**
+   * Check if user is an admin.
+   *
+   * @return bool
+   */
+  public function isAdmin()
+  {
+    return $this->is_admin;
+  }
 }
